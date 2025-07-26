9b3d6f9a24c1a32fdf593052499006a74ee4352e
CyberRookie-X <213730277+CyberRookie-X@users.noreply.github.com>
2025-07-26 15:52:18 +0800

    fix(firewallcard): fix rule cards width and gap issues

--- landscape-webui/src/components/firewall/FirewallRuleCard.vue
+++ landscape-webui/src/components/firewall/FirewallRuleCard.vue
@@ -17,44 +17,42 @@ async function del() {
 }
 </script>
 <template>
-  <n-flex>
-    <n-card :title="`优先级:${rule.index}`" size="small">
-      <!-- {{ rule }} -->
-      <n-descriptions bordered label-placement="top" :column="2">
-        <n-descriptions-item label="启用">
-          <span :style="{ color: rule.enable ? 'var(--success-color)' : '' }">
-            {{ rule.enable }}
-          </span>
-        </n-descriptions-item>
-        <!-- <n-descriptions-item label="流量标记">
-          {{ rule.mark }}
-        </n-descriptions-item> -->
-        <n-descriptions-item label="备注">
-          {{ rule.remark }}
-        </n-descriptions-item>
-        <n-descriptions-item label="匹配规则">
-          {{ rule.items }}
-        </n-descriptions-item>
-      </n-descriptions>
-      <template #header-extra>
-        <n-flex>
-          <n-button type="warning" secondary @click="show_edit_modal = true">
-            编辑
-          </n-button>
+  <n-card :title="`优先级:${rule.index}`" size="small" style="flex: 1">
+    <!-- {{ rule }} -->
+    <n-descriptions bordered label-placement="top" :column="2">
+      <n-descriptions-item label="启用">
+        <span :style="{ color: rule.enable ? 'var(--success-color)' : '' }">
+          {{ rule.enable }}
+        </span>
+      </n-descriptions-item>
+      <!-- <n-descriptions-item label="流量标记">
+        {{ rule.mark }}
+      </n-descriptions-item> -->
+      <n-descriptions-item label="备注">
+        {{ rule.remark }}
+      </n-descriptions-item>
+      <n-descriptions-item label="匹配规则">
+        {{ rule.items }}
+      </n-descriptions-item>
+    </n-descriptions>
+    <template #header-extra>
+      <n-flex>
+        <n-button type="warning" secondary @click="show_edit_modal = true">
+          编辑
+        </n-button>
 
-          <n-popconfirm @positive-click="del()">
-            <template #trigger>
-              <n-button type="error" secondary @click=""> 删除 </n-button>
-            </template>
-            确定删除吗
-          </n-popconfirm>
-        </n-flex>
-      </template>
-    </n-card>
-    <FirewallRuleEditModal
-      @refresh="emit('refresh')"
-      :rule="rule"
-      v-model:show="show_edit_modal"
-    ></FirewallRuleEditModal>
-  </n-flex>
+        <n-popconfirm @positive-click="del()">
+          <template #trigger>
+            <n-button type="error" secondary @click=""> 删除 </n-button>
+          </template>
+          确定删除吗
+        </n-popconfirm>
+      </n-flex>
+    </template>
+  </n-card>
+  <FirewallRuleEditModal
+    @refresh="emit('refresh')"
+    :rule="rule"
+    v-model:show="show_edit_modal"
+  ></FirewallRuleEditModal>
 </template>
